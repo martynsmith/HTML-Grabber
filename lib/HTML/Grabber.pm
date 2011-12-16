579af2c373264a520ff038fff9c1a8f01fac7ed5
Martyn Smith <martyn@dollyfish.net.nz>
2011-12-17 10:18:38 +1300

    Fixed synopsis.pl for new twitter layout

--- lib/HTML/Grabber.pm
+++ lib/HTML/Grabber.pm
@@ -35,11 +35,11 @@ HTML::Grabber
 
     my $dom = HTML::Grabber->new( html => get('http://twitter.com/ned0r') );
 
-    $dom->find('li.status')->each(sub {
-        my $body = $_->find('.entry-content')->text;
-        my $when = $_->find('.entry-date')->text;
-        my $link = $_->find('a[rel="bookmark"]')->attr('href');
-        say "$body $when (link: $link)";
+    $dom->find('.tweet-content')->each(sub {
+        my $body = $_->find('.tweet-text')->text;
+        my $when = $_->find('.js-tweet-timestamp')->attr('data-time');
+        my $link = $_->find('.js-permalink')->attr('href');
+        print "$body $when (link: $link)\n";
     });
 
 =head1 DESCRIPTION
--- synopsis.pl
+++ synopsis.pl
@@ -8,10 +8,10 @@ use LWP::Simple;
 
 my $dom = HTML::Grabber->new( html => get('http://twitter.com/ned0r') );
 
-$dom->find('li.status')->each(sub {
-    my $body = $_->find('.entry-content')->text;
-    my $when = $_->find('.entry-date')->text;
-    my $link = $_->find('a[rel="bookmark"]')->attr('href');
+$dom->find('.tweet-content')->each(sub {
+    my $body = $_->find('.tweet-text')->text;
+    my $when = $_->find('.js-tweet-timestamp')->attr('data-time');
+    my $link = $_->find('.js-permalink')->attr('href');
     print "$body $when (link: $link)\n";
 });
 
